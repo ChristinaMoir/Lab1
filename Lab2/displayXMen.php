3f472a8dddfcb9f5e61766ca59b0966180f1d078
Christina <1114409@rgu.ac.uk>
2016-10-10 14:46:22 +0100

    added files

---
+++ Lab2/displayXMen.php
@@ -0,0 +1,16 @@
+<?php
+include "../dbconnect.php";
+
+$sql_query = "SELECT * FROM marvelmovies WHERE title LIKE  '%X-Men%'";
+
+$results = $db->query($sql_query);
+
+while($row=$results->fetch_array()){
+    echo "<p> Title:".$row['title']." Year released: ".$row['yearReleased'].
+        " Production Studio: ".$row['productionStudio']." Notes: ".$row['notes']."</p>";
+}
+
+$results->close();
+$db->close();
+
+?>
---
+++ Lab2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<p>
+    <a href="displayAll.php">Display All Movies</a>
+    <a href="displayMarvel.php">Display All Movies created by Marvel Studios</a>
+    <a href="display2010.php">Display All Movies created after 2010</a>
+    <a href="displayXMen.php">Display All X-Men Films</a>
+</p>
+</body>
+</html>
