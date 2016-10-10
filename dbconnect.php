14b04c3f5dce5b1025fa85199bf167ccf6d7b7a5
Christina <1114409@rgu.ac.uk>
2016-10-10 14:21:56 +0100

    edited db file again

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,14 @@
 <?php
     $db = new mysqli(
-        'eu-cdbr-azure-north-e.cloudapp.net',
-        'bae6f2bb6b3381',
-        '17dce13b',
-        'mydb'
+        "eu-cdbr-azure-north-e.cloudapp.net",
+        "bae6f2bb6b3381",
+        "17dce13b",
+        "mydb"
     );
 
-
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}
 
 
 
